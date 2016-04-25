8d574a86799adc6e893d1ee9da6ca6913ec4442b
Lexan92 <luis_grennby92@hotmail.com>
2016-04-24 22:50:02 -0600

    Activity Consultar oferta agregada

---
+++ app/src/main/java/pdm/fia/ues/sv/bolsatrabajo/ConsultarOfertaActivity.java
@@ -0,0 +1,13 @@
+package pdm.fia.ues.sv.bolsatrabajo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsultarOfertaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_oferta);
+    }
+}
